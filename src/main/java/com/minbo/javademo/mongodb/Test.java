58ca5f9f76e1030a580133b25beeb93423aae385
minbo <hemin_it@163.com>
2017-03-17 00:00:26 +0800

    updae

--- src/main/java/com/minbo/javademo/mongodb/Test.java
+++ src/main/java/com/minbo/javademo/mongodb/Test.java
@@ -1,9 +1,21 @@
 package com.minbo.javademo.mongodb;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 public class Test {
 
 	public static void main(String[] args) {
+		try {
+			// 连接到 mongodb 服务
+			MongoClient mongoClient = new MongoClient("localhost", 27017);
 
-		
+			// 连接到数据库
+			MongoDatabase mongoDatabase = mongoClient.getDatabase("test");
+			System.out.println("Connect to database successfully");
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
 	}
 }
