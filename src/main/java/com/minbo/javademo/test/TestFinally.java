86e4531e418dac21a2fad59e3af449d4310b0e21
minbo <hemin_it@163.com>
2017-03-23 23:51:31 +0800

    update

--- src/main/java/com/minbo/javademo/test/TestFinally.java
+++ src/main/java/com/minbo/javademo/test/TestFinally.java
@@ -20,4 +20,8 @@ public class TestFinally {
 		}
 		System.out.printf("finished ");
 	}
+	
+//	1、finally块一定会执行，无论是否try…catch。
+//	2、finally前有return，会先执行return语句，并保存下来，再执行finally块，最后return。
+//	3、finally前有return、finally块中也有return，先执行前面的return，保存下来，再执行finally的return，覆盖之前的结果，并返回。
 }
