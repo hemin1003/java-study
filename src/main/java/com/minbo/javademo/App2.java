ebddbad1516b32e2bb35cfb0d242f7664e141ac4
贺敏 <hemin_it@163.com>
2017-03-29 09:07:07 +0800

    update

--- src/main/java/com/minbo/javademo/App2.java
+++ src/main/java/com/minbo/javademo/App2.java
@@ -11,5 +11,18 @@ public class App2
 //        char c = 253;
 //        int d = 12;
     	
+//    	String a = "test";
+//    	String b = new String("test");
+//    	System.out.println(a == b);
+//    	System.out.println(a.equals(b));
+    	
+//    	Integer c = new Integer(127);
+//    	int d = 127;
+//    	System.out.println(c == d);
+    	
+    	Integer e = new Integer(180);
+    	int f = 180;
+    	System.out.println(e == f);
+    	
     }
 }
