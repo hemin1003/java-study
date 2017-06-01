d395660b61ed600182eb6ab6ed4571d71b09c190
贺敏 <hemin_it@163.com>
2017-06-01 09:06:48 +0800

    update

--- src/main/java/com/minbo/javademo/bytes/Test10.java
+++ src/main/java/com/minbo/javademo/bytes/Test10.java
@@ -3,14 +3,15 @@ package com.minbo.javademo.bytes;
 public class Test10 {
 
 	public static void main(String[] args) {
-		String s1 = "贺敏";
+		String s1 = "2017-06-01 08:49:30";
 		byte[] b1 = s1.getBytes();
 		for (byte b : b1) {
-			System.out.println(b);
+			System.out.print(b + " ");
 		}
 		System.out.println();
 
-		byte[] b2 = new byte[]{-24, -76, -70, -26, -107, -113};
+//		byte[] b2 = new byte[]{-24, -76, -70, -26, -107, -113};
+		byte[] b2 = b1;
 		String s = new String(b2);
 		System.out.println(s);
 	}
