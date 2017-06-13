52ec6f36961e9fce100e5d4e15ede7dac1cd948b
贺敏 <hemin_it@163.com>
2017-06-13 18:08:16 +0800

    update

---
+++ src/main/java/com/minbo/javademo/elk/ElkLog4jTest.java
@@ -0,0 +1,21 @@
+package com.minbo.javademo.elk;
+
+import org.apache.log4j.Logger;
+
+public class ElkLog4jTest {
+
+	private static final Logger logger = Logger.getLogger(ElkLog4jTest.class);
+
+	public static void main(String[] args) {
+		logger.debug("This is a debug message!");
+		logger.info("This is info message!");
+		logger.warn("This is a warn message!");
+		logger.error("This is error message!");
+
+		try {
+			System.out.println(5 / 0);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+	}
+}
