dfbbd63e081586c80ec57553ee30b6fa649572fc
贺敏 <hemin_it@163.com>
2017-07-05 17:20:01 +0800

    update

--- src/main/java/com/minbo/javademo/elk/ElkLog4jTest.java
+++ src/main/java/com/minbo/javademo/elk/ElkLog4jTest.java
@@ -7,7 +7,7 @@ public class ElkLog4jTest {
 	private static final Logger logger = Logger.getLogger(ElkLog4jTest.class);
 
 	public static void main(String[] args) {
-		logger.debug("This is a debug message of google!");
+		logger.debug("This is a debug message of minbo!");
 		logger.info("This is info message!");
 		logger.warn("This is a warn message!");
 		logger.error("This is error message!");
